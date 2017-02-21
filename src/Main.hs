2655016b7c7ee67e86556a70bcb7e86bb2c10945
Bertram Felgenhauer <int-e@gmx.de>
2017-02-21 21:39:53 +0100

    add support for .xml TPDB files

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,38 @@
 module Main where
 
 import Canonicalize
-import System.Environment
-import Data.List
 import qualified Data.Rewriting.Problem as Problem
+import qualified Data.Rewriting.Problem.Xml as Problem
 import qualified Text.PrettyPrint.ANSI.Leijen as Pretty
+import System.Environment
+import System.FilePath
+import System.Exit
+import System.IO
 
 main :: IO ()
 main = do
-    [fn] <- getArgs
-    Right problem <- Problem.fromFile fn
-    putStrLn $ ($ "") $ Pretty.displayS $ Pretty.renderCompact $ Problem.prettyWST Pretty.text Pretty.text $ canonicalize problem
+    args <- getArgs
+    let fn = head args
+        ext = takeExtension fn
+    case (args, ext) of
+        ([_], ".trs") -> do
+            res <- Problem.fromFile fn
+            case res of
+                Right problem -> process problem
+                Left error -> do
+                    hPutStrLn stderr $ show error
+                    exitFailure
+        ([_], ".xml") -> do
+            problem <- Problem.xmlFileToProblem fn
+            process problem
+        _ -> do
+            prog <- getProgName
+            hPutStrLn stderr $ "Usage: " ++ prog ++ " [FILE.trs|FILE.xml]"
+            exitFailure
+
+process :: Problem.Problem String String -> IO ()
+process =
+    putStrLn .
+    ($ "") . Pretty.displayS . Pretty.renderCompact .
+    Problem.prettyWST Pretty.text Pretty.text .
+    canonicalize
